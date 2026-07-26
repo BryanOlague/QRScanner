a76e9ec61f0a743e5d4d83b798a4e4af0cbe0197
Uriel Olague <uriel_olague2018@outlook.com>
2026-07-26 13:57:12 -0600

    Creaccion componente RegisterCard

--- src/routers/AppRouter.tsx
+++ src/routers/AppRouter.tsx
@@ -1,14 +1,16 @@
 import { Route, Routes } from "react-router-dom";
 import QRScanner from "../components/QRScanner";
 import Sidebar from "../components/SideBar";
+import RegisterCard from "../components/RegisterCard";
 
 export default function AppRouter() {
   return (
     <>
       <Routes>
-        <Route path="/" element={<p>Inicio</p>} />
+        <Route path="/" element={<p>Inico</p>} />
         <Route path="/qrScanner" element={<QRScanner />} />
         <Route path="/information" element={<Sidebar />} />
+        <Route path="/register" element={<RegisterCard />} />
       </Routes>
     </>
   );
